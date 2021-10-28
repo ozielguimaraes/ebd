0970186dec3e24f5b4cb9e9a06bd5d27a192f41e
Oziel Guimarães de Paula Silva <hi@ozielguimaraes.dev>
2021-10-28 17:40:40 -0300

    Updating toolsVersion

--- EscolaBiblicaDominical/Ebd.Presentation.Api/Startup.cs
+++ EscolaBiblicaDominical/Ebd.Presentation.Api/Startup.cs
@@ -2,18 +2,11 @@ using Ebd.CrossCutting.IoC;
 using Ebd.Presentation.Api.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text.Json.Serialization;
-using System.Threading.Tasks;
 
 namespace Ebd.Presentation.Api
 {
