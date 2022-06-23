658e3ed2a1aab307718b9156dca79da763926b0f
Oziel Guimarães de Paula Silva <hi@ozielguimaraes.dev>
2022-06-23 17:02:45 -0300

    Adicionar migration para adicionar avaliações

---
+++ EscolaBiblicaDominical/Ebd.Infra.Data/Migrations/20220623193057_InsertAvaliacao.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ebd.Infra.Data.Migrations
+{
+    public partial class InsertAvaliacao : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Avaliacao",
+                columns: new[] { "AvaliacaoId", "Nome", "Nota" },
+                values: new object[,]
+                {
+                    { 1, "Presença", 2 },
+                    { 2, "Trouxe visita?", 5 },
+                    { 3, "Trouxe lição?", 2 },
+                    { 4, "Trouxe bíblia?", 3 },
+                    { 5, "Trouxe oferta?", 1 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Avaliacao",
+                keyColumn: "AvaliacaoId",
+                keyValue: 1);
+            migrationBuilder.DeleteData(
+                table: "Avaliacao",
+                keyColumn: "AvaliacaoId",
+                keyValue: 2);
+            migrationBuilder.DeleteData(
+                table: "Avaliacao",
+                keyColumn: "AvaliacaoId",
+                keyValue: 3);
+            migrationBuilder.DeleteData(
+                table: "Avaliacao",
+                keyColumn: "AvaliacaoId",
+                keyValue: 4);
+            migrationBuilder.DeleteData(
+                table: "Avaliacao",
+                keyColumn: "AvaliacaoId",
+                keyValue: 5);
+        }
+    }
+}
