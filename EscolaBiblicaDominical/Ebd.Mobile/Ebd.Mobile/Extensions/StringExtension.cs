72fc3270929ddfeb79466c0860b947b03aea1f54
Oziel Guimarães de Paula Silva <hi@ozielguimaraes.dev>
2023-11-21 08:01:21 -0300

    Adicionando método ToTitleCase

--- EscolaBiblicaDominical/Ebd.Mobile/Ebd.Mobile/Extensions/StringExtension.cs
+++ EscolaBiblicaDominical/Ebd.Mobile/Ebd.Mobile/Extensions/StringExtension.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Globalization;
+using System.Linq;
 
 namespace Ebd.Mobile.Extensions
 {
@@ -10,9 +11,13 @@ namespace Ebd.Mobile.Extensions
 
             return digits.Length > 0 ? new string(digits) : string.Empty;
         }
+
         public static string DigitsOnly(this string input) => input.NumbersOnly();
+
         public static string FirstName(this string input) => input.Substring(0, input.IndexOf(' ')).ToLower().FirstToUpper();
         public static string FirstToUpper(this string input)
             => input.Length <= 1 ? input : input.Substring(0, 1).ToUpper() + input.Substring(1).ToLower();
+
+        public static string ToTitleCase(this string input) => CultureInfo.CurrentCulture.TextInfo.ToTitleCase(input.ToLower());
     }
 }
