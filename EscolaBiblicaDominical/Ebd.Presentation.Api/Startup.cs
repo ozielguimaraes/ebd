fce771e71fa15ec9b3c6af27bd714ed2bf0823bf
Oziel Guimarães de Paula Silva <oziel@gliconline.com.br>
2023-11-27 08:58:25 -0300

    Alterando configuração do swagger

--- EscolaBiblicaDominical/Ebd.Presentation.Api/Startup.cs
+++ EscolaBiblicaDominical/Ebd.Presentation.Api/Startup.cs
@@ -33,6 +33,7 @@ namespace Ebd.Presentation.Api
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Ebd.Presentation.Api", Version = "v1" });
+
             });
         }
 
@@ -47,7 +48,7 @@ namespace Ebd.Presentation.Api
             app.UseSwagger();
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Ebd.Presentation.Api v1");
+                c.SwaggerEndpoint($"{Configuration["AppPath"]}/swagger/v1/swagger.json", "Ebd.Presentation.Api v1");
                 c.RoutePrefix = string.Empty;
             });
 
