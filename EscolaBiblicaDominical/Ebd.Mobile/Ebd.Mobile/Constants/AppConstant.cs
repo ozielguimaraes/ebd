05a4807bd73190ab5f151c1e3ef11c12d3eb98b2
Oziel Guimarães de Paula Silva <oziel@gliconline.com.br>
2023-12-02 08:12:12 -0300

    Alterando endereço

--- EscolaBiblicaDominical/Ebd.Mobile/Ebd.Mobile/Constants/AppConstant.cs
+++ EscolaBiblicaDominical/Ebd.Mobile/Ebd.Mobile/Constants/AppConstant.cs
@@ -2,7 +2,7 @@
 {
     internal class AppConstant
     {
-        public const string BaseUrl = "http://asser.site/api";
+        public const string BaseUrl = "http://209.23.8.169/ebd/api";
         //public const string BaseUrl = "https://8d8f-2804-14d-a4ba-8028-1d7b-376c-6f17-c6a1.ngrok-free.app/api";
     }
 }
