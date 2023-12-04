dfff60cf986e1c74972b225ec2ca19f2b398099b
Oziel Guimarães de Paula Silva <hi@ozielguimaraes.dev>
2023-12-04 07:27:07 -0300

    header

--- EscolaBiblicaDominical/Ebd.Mobile/Ebd.Mobile/Services/Implementations/Base/ApiService.cs
+++ EscolaBiblicaDominical/Ebd.Mobile/Ebd.Mobile/Services/Implementations/Base/ApiService.cs
@@ -39,7 +39,6 @@ namespace Ebd.Mobile.Services.Implementations.Base
             };
 
             HttpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(ApplicationJson));
-            HttpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36");
 
             _jsonSerializerOptions = new JsonSerializerOptions
             {
