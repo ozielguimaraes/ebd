5189ced06e7ef4734faf1771dfba9ad103d2316f
Oziel Guimarães de Paula Silva <hi@ozielguimaraes.dev>
2023-12-27 18:07:16 -0300

    adding FilePathManager to android

---
+++ EscolaBiblicaDominical/Ebd.Mobile/Ebd.MobileApp/Platforms/Android/Infrastructure/FilePathManager.cs
@@ -0,0 +1,15 @@
+﻿using Ebd.Mobile.Droid.Infrastructure;
+using Ebd.Mobile.Infrastructure;
+
+[assembly: Dependency(typeof(FilePathManager))]
+namespace Ebd.Mobile.Droid.Infrastructure
+{
+    public class FilePathManager : IFilePath
+    {
+        public string GetFullPath(string fileName)
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            return Path.Combine(path, fileName);
+        }
+    }
+}
